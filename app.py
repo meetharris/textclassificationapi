a18c75f6d5e065153a559c6f107e58c05c96ccf0
Vaishnavi Parab <68656329+VaishnaviParab01@users.noreply.github.com>
2023-04-21 21:56:15 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,69 +1,32 @@
-from fastapi import FastAPI
-import uvicorn
-from fastapi import FastAPI, Request, status, File, Form, UploadFile, Response
-from fastapi.encoders import jsonable_encoder
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponse
-from fastapi.middleware.cors import CORSMiddleware
+import os
 
-app = FastAPI()
-origins = ["*"]
+from flask import (Flask, redirect, render_template, request,
+                   send_from_directory, url_for)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-'''
-@app.get("/items/{item_id}")
-async def read_id(item_id: int):
-    print(item_id)
-    import copy_Recognize_face_image
-    copy_Recognize_face_image.compare_faces(item_id)
-    return {"item_id": item_id}
-'''
-
-@app.post('/get_info/')
-async def read_image(file: UploadFile = File(...), item_id: int = Form(...)):
-    contents = await file.read()
-    print(type(contents))
-    #print(item_id)
-    # print(file)
-    import cv2
-    import numpy as np
-    decoded_image = cv2.imdecode(np.frombuffer(contents, np.uint8), -1)
-    print(type(decoded_image))
-    from Recognize_face_image import compare_faces
-    result, execution = compare_faces(item_id, decoded_image)
-    execution = str(round(execution, 2))
-    print(result)
-    print(execution+'s')
-    return {'Result': result, 'executionTime': execution + 's'}
-
-@app.post('/predict_Category/')
-async def read_csv(file: UploadFile = File(...), item_id: int = Form(...)):
-    contents = await file.read()
-    print(type(contents))
+app = Flask(__name__)
 
 
-    #print(item_id)
-    print(file)
-    return {'Result': contents, 'executionTime': 'executionTime'}
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
 
-    #import cv2
-    #import numpy as np
-    #decoded_image = cv2.imdecode(np.frombuffer(contents, np.uint8), -1)
-    #print(type(decoded_image))
-    #from Recognize_face_image import compare_faces
-    #result, execution = compare_faces(item_id, decoded_image)
-    #execution = str(round(execution, 2))
-    #print(result)
-    #print(execution+'s')
-    #return {'Result': result, 'executionTime': execution + 's'}
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
+
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, port=8000, host="0.0.0.0")
+   app.run()
---
+++ app_fast.py
@@ -0,0 +1,73 @@
+from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, Request, status, File, Form, UploadFile, Response
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+'''
+@app.get("/items/{item_id}")
+async def read_id(item_id: int):
+    print(item_id)
+    import copy_Recognize_face_image
+    copy_Recognize_face_image.compare_faces(item_id)
+    return {"item_id": item_id}
+'''
+@app.get("/myname")
+async def read_id(name: str):
+    print(name)
+    return {"name": name}
+
+@app.post('/get_info/')
+async def read_image(file: UploadFile = File(...), item_id: int = Form(...)):
+    contents = await file.read()
+    print(type(contents))
+    #print(item_id)
+    # print(file)
+    import cv2
+    import numpy as np
+    decoded_image = cv2.imdecode(np.frombuffer(contents, np.uint8), -1)
+    print(type(decoded_image))
+    from Recognize_face_image import compare_faces
+    result, execution = compare_faces(item_id, decoded_image)
+    execution = str(round(execution, 2))
+    print(result)
+    print(execution+'s')
+    return {'Result': result, 'executionTime': execution + 's'}
+
+@app.post('/predict_Category/')
+async def read_csv(file: UploadFile = File(...), item_id: int = Form(...)):
+    contents = await file.read()
+    print(type(contents))
+
+
+    #print(item_id)
+    print(file)
+    return {'Result': contents, 'executionTime': 'executionTime'}
+
+    #import cv2
+    #import numpy as np
+    #decoded_image = cv2.imdecode(np.frombuffer(contents, np.uint8), -1)
+    #print(type(decoded_image))
+    #from Recognize_face_image import compare_faces
+    #result, execution = compare_faces(item_id, decoded_image)
+    #execution = str(round(execution, 2))
+    #print(result)
+    #print(execution+'s')
+    #return {'Result': result, 'executionTime': execution + 's'}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8000, host="0.0.0.0")
